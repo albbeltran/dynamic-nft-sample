90a85c7aa43b0d2cb5eef01a21be3ae0cb4e509b
Alberto Beltran <albertobeltran@protonmail.com>
2022-08-21 13:53:38 -0500

    initial commit - static NFT

---
+++ contracts/nft.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract EthDevProgram is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    string uriData = "https://gateway.pinata.cloud/ipfs/QmdxKue9tb8v7ocFH6jtoyZGDYNgzXZpKjSNZ572hfFF8F";
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("EthDevProgram", "EDP") {}
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uriData);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+}
